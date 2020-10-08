f9def73d81738d6e3e17124ecd0b099cc0315634
Nick Norman <nman976@gmail.com>
2020-10-08 16:58:58 -0400

    I think I uploaded the homework files

---
+++ norman_p3/src/Shape.java
@@ -0,0 +1,17 @@
+abstract class Shape {
+    String breed;
+
+    public void bark(){
+        System.out.println("Bark!");
+    }
+
+
+}
+
+public class Shape {
+    public void main(String[] args){
+        Shape s = new Shape();
+
+
+    }
+}
